991ffe6854ef50d59365db84ac0abe4c4b5ce100
AndyLiming <mingli6250@163.com>
2020-11-21 20:08:58 +0800

    updated C++ template of BIT and UF

--- codingTemplate/codingTemplate/dsTemplate.hpp
+++ codingTemplate/codingTemplate/dsTemplate.hpp
@@ -4,20 +4,50 @@ using namespace std;
 
 //Binary Indexed Tree 树状数组
 //能够以O(logN)得到任意前缀和（区间和）
-//template<typename T>
+template<typename T>
 class BIT {
 private:
   int n;
-  vector<int> tree;
+  vector<T> tree;
 public:
   BIT(int _n):n(_n),tree(_n+1){}
   int lowbit(int x) {
     return x & (-x);
   }
-  void update(int x) {
-
+  void add(int x,T k) {
+    while (x <= n) {
+      tree[x]+=k;
+      x += lowbit(x);
+    }
   }
-  int query(int x) {
+  T query(int x) {
+    T ans = 0;
+    while (x) {
+      ans += tree[x];
+      x -= lowbit(x);
+    }
+    return ans;
+  }
+};
 
+//Union-Find 并查集
+class UF {
+private:
+  int n;
+  vector<int>parent;
+public:
+  UF(int _n) {
+    n = _n;
+    parent = vector<int>(_n);
+    for (int i = 0; i < n; ++i) parent[i] = i;
+  }
+  int getParent(int v) {
+    if (parent[v] == v) return v;
+    else return parent[v];
+  }
+  void merge(int v, int u) {
+    int fv = getParent(v), fu = getParent(u);
+    parent[fu] = fv;
+    return;
   }
 };
